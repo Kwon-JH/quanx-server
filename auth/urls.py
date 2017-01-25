8693f55ef70ea5a130eea6cca10bba6331b47392
Yoonje Choi <yoonjechoi@gmail.com>
2017-01-25 17:47:58 +0900

    refactor(auth): organize imports
    Organize imports

--- auth/urls.py
+++ auth/urls.py
@@ -1,12 +1,6 @@
 from django.conf.urls import url, include
-from django.contrib import admin
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
-
